e6ab8d2d82263e8ce3004c1d47ea6ca685dad554
Yelyzaveta <elizabethcalmau@gamil.com>
2023-11-06 17:17:51 +0200

    added photos and block for portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css';
 import { Sidebar } from './sections/sidebar';
-
+import { Portfolio } from './sections/portfolio/Portfolio';
 
 function App() {
   return (
     <>  
-    hello
+  <Portfolio/>
   <Sidebar/>
-
     </>
   )
 }
---
+++ src/sections/portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import './Portfolio.css'
+
+export const Portfolio = () => {
+    return(
+        <div className='portfolio_wrapper'>
+            <div className='portfolio_ph_1'></div>
+            <div className='portfolio_ph_2'></div>
+            <div className='portfolio_ph_3'></div>
+        </div>
+    )
+}
