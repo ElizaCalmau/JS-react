68c1ad6b26d4b3a71562b99cfef73dcd216c29be
Yelyzaveta <elizabethcalmau@gamil.com>
2023-11-10 14:52:42 +0200

    add toggle to button text

--- src/sections/changableBlock/ChangableBlock.jsx
+++ src/sections/changableBlock/ChangableBlock.jsx
@@ -6,21 +6,30 @@ import { useState } from 'react'
 export const ChangableBlock = () => {
     const [block, setBlock] = useState(<Info/>);
 
-    function handleCLick () {
-
-        block.type.name == 'Info' ? setBlock(<Portfolio/>) :setBlock(<Info/>);
+    function handleCLick (e) {
+        block.type.name == 'Info' ? setBlock(<Portfolio/>):setBlock(<Info/>);
+        console.log(e.target.innerHTML);
+        console.log(e)
         handleToggle();
+        handleText();
     }
 
     const [isActive, setActive] = useState("false");
+
     const handleToggle = () => {
         setActive(!isActive);
       };
 
+    const [text, setText] = useState('See Portfolio');
+
+    const handleText = () => {
+        text == 'See Portfolio' ? setText('See Bio') : setText('See Portfolio');
+    }
+
     return(
         <div className="changable_block">
             {block}
-            <button className={isActive? 'changable_button' : 'unfocus'} onClick={handleCLick}>See Portfolio</button>
+            <button className= { isActive? 'unclicked_button' : 'clicked_button'} onClick={handleCLick}>{text}</button>
         </div>
     )
 }
