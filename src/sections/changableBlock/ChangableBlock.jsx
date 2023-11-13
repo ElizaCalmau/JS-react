83896cda226e43880c1534f0e958c2ce869f5de8
Yelyzaveta <elizabethcalmau@gamil.com>
2023-11-13 12:09:34 +0200

    reduced handleToggle

--- src/sections/changableBlock/ChangableBlock.jsx
+++ src/sections/changableBlock/ChangableBlock.jsx
@@ -6,20 +6,15 @@ import './ChangableBlock.css'
 export const ChangableBlock = () => {
     const [showPortfolio, setShowPortfolio] = useState(false);
     const [text, setText] = useState('See Portfolio');
-    const [isActive, setActive] = useState(true);
 
     function handleCLick () {
         setShowPortfolio((prevState)=> {
             return !prevState;
         })
        
-        //handleToggle();
-        //handleText();
+        handleToggle();
+        handleText();
     }
-
-    function handleToggle () {
-        setActive(!isActive);
-      }
 
     function handleText () {
         text == 'See Portfolio' ? setText('See Bio') : setText('See Portfolio');
@@ -27,8 +22,8 @@ export const ChangableBlock = () => {
 
     return(
         <div className="changableBlock">
-            {showPortfolio ? <Portfolio/> : <Info/>}
-            <button className= { isActive? 'unclickedButton' : 'clickedButton'} onClick={handleCLick}>{text}</button>
+            { showPortfolio ? <Portfolio/> : <Info/> }
+            <button className= { showPortfolio? 'clickedButton' : 'unclickedButton'} onClick={handleCLick}>{text}</button>
         </div>
     )
 }
