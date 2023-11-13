a79e1f950f729d04f00d3f946056af0efe2901c9
Yelyzaveta <elizabethcalmau@gamil.com>
2023-11-13 16:13:50 +0200

    to do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 import { Button } from './components/button'
 import './App.css'
 
+
 function App() {
+  const [value, setValue] = useState('');
+  const [storage, setStorage] = useState([]);
+  console.log(storage)
   return (
     <>
       <Button />
+
+      <div className='to-do-list'>
+        <input onChange={(e)=>{
+          setValue(e.target.value);
+        }}/>
+        <button onClick={()=> {
+          const newStorage = [...storage, value];
+          setStorage(newStorage)
+        }}> Add </button>
+        <ul>
+    { storage.map((el, index)=> {
+        return(
+            <li key = {index}> {el} </li>
+        )
+    })}
+    </ul>
+      </div>
     </>
   )
 }
