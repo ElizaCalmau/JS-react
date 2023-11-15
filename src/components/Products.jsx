616896baf520c3c4306f8dfd7aaa167ccbb94cc5
Yelyzaveta <elizabethcalmau@gamil.com>
2023-11-15 19:14:38 +0200

    made geolocation rendering before the fetch

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,22 +1,35 @@
 import { useEffect, useState } from "react";
 
 export const Products = () => {
-    const [post, setPost] = useState([]);
-    useEffect(()=> {
-        fetch('https://dummyjson.com/posts')
-        .then(res => res.json())
-        .then(data => 
-            setPost(data.posts)
-            )      
-    }, [])
-    
+  const [post, setPost] = useState([]);
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+  useEffect(() => {
+    if (post.length < 1) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          console.log(position);
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          setLocation({ latitude, longitude });
+        });
+      }
+    }
 
-    return(
-        <div>
-            {post.map((el)=>{
-                return <p key={el.id}> - {el.title} -</p>
-            })}
-        </div>
-    )
-}
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPost(data.posts));
+  }, [post.length]);
 
+  return (
+    <>
+      <div>
+        {post.map((el) => {
+          return <p key={el.id}> - {el.title} -</p>;
+        })}
+      </div>
+      <h4>
+        Latitude: {location.latitude}, longitude: {location.longitude}
+      </h4>
+    </>
+  );
+};
