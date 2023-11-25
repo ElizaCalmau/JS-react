d8b0f95fbebc931ea94df64f8641b9a1f3017af1
Yelyzaveta <elizabethcalmau@gamil.com>
2023-11-25 11:59:56 +0200

    fixed multiple fetch requests by adding useEffect

--- src/section/Posts.jsx
+++ src/section/Posts.jsx
@@ -1,19 +1,24 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../provider/UserProvider";
 import './Posts.css'
 
 export const Posts = () => {
     const {log} = useContext(UserContext);
-        const [posts, setPosts]=useState([])
-    fetch('https://dummyjson.com/posts')
-    .then(res => res.json())
-    .then(data => setPosts(data.posts));
-    const post = posts.map((p)=>{
+    const [posts, setPosts]=useState([])
+    useEffect(()=>{
+        if(log === 'logged'){
+            fetch('https://dummyjson.com/posts')
+            .then(res => res.json())
+            .then(data => setPosts(data.posts));
+        }
+    }, [log])
+
+    const postRender = posts.map((p)=>{
         return <p key={p.id}> {p.title}</p>
     })
     return(
         <div>
-        {log === 'logged' ? post : 'log in pls'}
+        {log === 'logged' ? postRender : 'log in pls'}
     </div>
     ) 
     }
