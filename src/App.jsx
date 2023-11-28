4081ce59c73163cb4ace1d27f570f0b63b76e076
Yelyzaveta <elizabethcalmau@gamil.com>
2023-11-28 14:14:09 +0200

    homework #40 useref hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-
+import {useRef, useState} from 'react'
 import './App.css'
+import { ColorPicker } from './components/ColorPicker'
 
 function App() {
+  const ref = useRef(null)
+  const [color, setColor] = useState()
   return (
     <>
-        
+        <ColorPicker ref={ref}/>
+        <div>
+          {color}
+        <button onClick={()=> {
+          setColor(ref.current)
+        }}>click</button>
+        </div>
     </>
   )
 }
