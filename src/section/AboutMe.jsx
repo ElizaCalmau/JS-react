76b8e321b2b7ec71b62425086b3db6a95f957a9a
Yelyzaveta <elizabethcalmau@gamil.com>
2023-11-30 11:46:09 +0200

    add cv photo

--- src/section/AboutMe.jsx
+++ src/section/AboutMe.jsx
@@ -1,6 +1,8 @@
+import './AboutMe.css'
+
 export const AboutMe = () => {
     return<main>
-        <div className="ph"> </div>
+        <div className="myPhoto"> </div>
          <h2>Yelyzaveta Lysenko</h2>
          <h3>Trainee/Junior Front-End Devepoler</h3>
          </main>
