e686ce7e26e2aeb97b9e070de5271fb2af850ff5
Yelyzaveta <elizabethcalmau@gamil.com>
2023-11-30 10:48:21 +0200

    add ph

---
+++ src/section/AboutMe.jsx
@@ -0,0 +1,7 @@
+export const AboutMe = () => {
+    return<main>
+        <div className="ph"> </div>
+         <h2>Yelyzaveta Lysenko</h2>
+         <h3>Trainee/Junior Front-End Devepoler</h3>
+         </main>
+}
