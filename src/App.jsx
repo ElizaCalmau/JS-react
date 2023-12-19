6f90398c9d2eccaa018ab10974905dcdedab394c
Yelyzaveta <elizabethcalmau@gamil.com>
2023-12-19 17:01:53 +0200

    api requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useState } from 'react'
+
+const apiKey = '499adc143510099427a185e36cd48fb2'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([])
+  const onTopRated = () => {
+      fetch( `https://api.themoviedb.org/3/movie/top_rated?api-key=${apiKey}`)
+        .then( res => res.json())
+        .then( data => setMovies(data.results))
+  }
+    
+    const onPopular = () => {
+      fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+      .then(res => res.json())
+      .then (data => setMovies(data.results))
+    }
+
+    const onUpcoming = () => {
+      fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`)
+      .then(res => res.json())
+      .then (data => setMovies(data.results))
+    }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <button onClick={onPopular}>Popular</button>
+      <button onClick={onTopRated}>Top rated</button>
+      <button onClick={onUpcoming}>Upcoming</button>
+      {movies.map((movie) => 
+        <div key={movie.id}>
+          <p>{movie.title}</p>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+        </div>)}
+    </div>
     </>
   )
-}
-
+      }
 export default App
