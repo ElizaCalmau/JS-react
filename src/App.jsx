a4cecc0dd0212fc4e1b57d4e9611702d0e76d509
Yelyzaveta <elizabethcalmau@gamil.com>
2024-01-04 11:56:23 +0200

    class cmps

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import { useState } from 'react'
 import {Button} from './cmps/Button'
 import './App.css'
-import { SingUpForm } from './SignUpForm'
+
 
 function App() {
-let [count, setCount] = useState('')
-const [disabled, setDisabled] =useState(true)
+
   return(
     <>
-    <input value={count} onChange={(e)=>{
-      if(e.target.value.length < 5){
-      setCount(e.target.value);
-      }
-    }}/>
-    <div>
-      <input type='number' onBlur={(e)=>{
-        const num = +e.target.value 
-        setDisabled(num < 18 ? true : false)
-      }}/>
-      <Button value={disabled}/>
-      <SingUpForm/>
-    </div>
+      <Button/>
     </>
   )
 }
--- src/cmps/Button.jsx
+++ src/cmps/Button.jsx
@@ -1,3 +1,28 @@
-export const Button = ({value}) => {
-    return <button disabled={value}>click</button>
+import React from "react";
+
+export class Button extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = { count : 1}
+        console.log('constructor')
+    }
+    componentDidMount(){
+        console.log('mount')
+    }
+    componentDidUpdate(...rest){
+        console.log(rest)
+    }
+    onClick = () => {
+        const count = this.state.count + 1;
+        this.setState({ count })
+    }
+    render(){
+        console.log('render')
+        console.log(this.state)
+        return(
+            <button onClick={this.onClick}>
+              click
+            </button>
+        )
+    }
 }
